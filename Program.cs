a2dbb7af13f7bc901c7f99f556b2f3db1c87a7bd
Michael Hodgson <michodgs@gmail.com>
2021-09-15 15:52:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
