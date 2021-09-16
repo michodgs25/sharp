a4dbab5e2f9deebb8b8f8c6b0b7bad43c2bba8a6
Michael Hodgson <michodgs@gmail.com>
2021-09-16 21:12:15 +0100

    Create double& float data type variables

--- Program.cs
+++ Program.cs
@@ -14,11 +14,30 @@ namespace hello_Sharp
             num1 = 13;
             // declaring and initializing a variable in one go
             int num2 = 11;
-            int sum = num1 + num2;
-
+      
+            num2 = 100;
+            
             // using concatination
             Console.WriteLine("num1 is " + num1);
+            int sum = num1 + num2;
             Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " is " + sum);
+
+            // using double numbers
+            double d1 = 3.1415;
+            double d2 = 5.1;
+            double dDiv = d1 / d2;
+            Console.WriteLine("d1/d2 is " + dDiv);
+
+            // using float point numbers
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+            Console.WriteLine("f1/f2 is " + fDiv);
+
+            double dIDiv = d1 / num1;
+            Console.WriteLine("d1/num2 is " + dIDiv);
+
+            // declaring multiple variables at once
             Console.Read();
         }
     }
