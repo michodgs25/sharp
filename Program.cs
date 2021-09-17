f2addf5aa3b86696d339ae1a3184b4da6b5a9f05
Michael Hodgson <michodgs@gmail.com>
2021-09-17 15:25:44 +0100

    Create implicit, explicit and typeConversion examples

--- Program.cs
+++ Program.cs
@@ -12,8 +12,38 @@ namespace hello_Sharp
         // entry point of program
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
+           // Console.ForegroundColor = ConsoleColor.Red;
+           // Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
             Console.WriteLine("Hello Michael");
+
+            //implicit conversion
+            int num = 123455566;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.17;
+            int myInt;
+
+
+            // cast double to int;
+            // Explicit conversion
+            myInt = (int)myDouble;
+
+            // typeConversion
+            string myString = myNewDouble.ToString();
+            num.ToString();
+            string myFloatString = myFloat.ToString();
+
+            bool sunIsShining = true;
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(sunIsShining);
+            Console.WriteLine(myFloatString);
+            Console.WriteLine(myInt);
+            Console.Read();
         }
     }
 }
