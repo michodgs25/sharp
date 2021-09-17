fb1fcd358e0142648953567526fc1e97d58e7350
Michael Hodgson <michodgs@gmail.com>
2021-09-17 16:18:48 +0100

    Create four examples of string manipulation

--- Program.cs
+++ Program.cs
@@ -63,6 +63,33 @@ namespace hello_Sharp
             Console.WriteLine("String Formatting");
             Console.WriteLine("Hello my name is {0} , I am {1} years old , I work as a {2} and I have been for over {3} year.", name, age, occupation, length);
 
+            //3. String interpolation
+            // variables like this {variablesName}
+            Console.WriteLine("StringInterpolation");
+            Console.WriteLine($"My name is {name}, I am {age} years old, I work as a {occupation} and I have been doing this for over {length} year.");
+
+            //4. Verbatim strings
+            //literally and ignore any spaces& escape characters like \n
+            Console.WriteLine("Verbatim strings");
+            Console.WriteLine(@"Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                                
+                                It has survived not only five centuries, 
+                                but also the leap into electronic typesetting,
+                                remaining essentially unchanged. 
+                                
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                                and more recently with desktop publishing software
+                                like Aldus PageMaker including versions of Lorem Ipsum."
+                                );
+            //instead of using \\ to write file paths we can use verbatim strings to make it easier
+            //if you remove the @ you will get an error because \U , \A and \D are not valid escape characters
+            Console.WriteLine(@"C:\User\Admin\Dog Pictures\Dog1\Super CoolDog.png");
+
+            //with verbatim strings even valid escape characters won't work
+            Console.WriteLine(@"Muhahaha \n you have no powers here.");
+
             //Console.WriteLine(resultInt);
             //Console.WriteLine(result);
             //Console.WriteLine(myBoolString);
