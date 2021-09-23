74d68bb9c818eef9fd56ead91d1614a33d3bbd73
Michael Hodgson <michodgs@gmail.com>
2021-09-23 16:28:07 +0100

    Create method file& explain c# method purpose

---
+++ methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*A method is a code block that contains a series of statements.
+             A program causes the statements to be executed,
+            by calling the method and specifying any required method arguments.
+            
+            In C#, every executed instruction is performed in the context of a method.
+            The Main method is the entry point for every C# application,
+            and is called by common language runtime CLR, when the program is started.*/
+        }
+    }
+}
